ea43eed7a4ced7eb12f206d29b6b023dbfc68f11
Harvard-Westlake <github@hw.com>
2023-02-18 14:46:42 -0800

    Arm Helper Classes

---
+++ src/main/java/frc/robot/Arm/ArmCalculator.java
@@ -0,0 +1,17 @@
+package frc.robot.Arm;
+
+import java.math.*;
+
+// store all calculations here
+
+public class ArmCalculator {
+    
+    // return max rotation given length
+
+    // return max length given rotation
+    public double maxLength (double angle)
+    {
+        
+    }
+
+}
---
+++ src/main/java/frc/robot/Arm/ArmConstants.java
@@ -0,0 +1,18 @@
+package frc.robot.Arm ;
+
+// once complete, combine with Constants.java
+
+public class ArmConstants
+{
+    public static final double ROTATE_ABS_MAX = 120 ; // degrees
+        // absolutely max angle for arm (fully retracted and towards ground)
+
+    //! MUST REMEASURE VIA CAD OR ROBOT
+
+    public static final double LENGTH_ABS_MAX = 60 ; // in
+        // absolutely max length for arm in any state (hypotenuse of 48in x (maxHeight)in triangle)
+    public static final double LENGTH_ABS_MIN = 35 ; // in
+        // absolutely min length in any state (fully retracted)
+
+
+}
