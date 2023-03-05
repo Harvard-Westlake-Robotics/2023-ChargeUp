23cfa523a0442e95efe64b3d3fa085866bf05a80
Harvard-Westlake <github@hw.com>
2023-03-04 16:52:41 -0800

    update limelight

--- src/main/java/frc/robot/DriverStation/LimeLight.java
+++ src/main/java/frc/robot/DriverStation/LimeLight.java
@@ -1,45 +1,51 @@
-// package frc.robot.DriverStation;
+package frc.robot.DriverStation;
 
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.networktables.NetworkTable;
-// import edu.wpi.first.networktables.NetworkTableEntry;
-// import edu.wpi.first.networktables.NetworkTableInstance;
-// import oi.limelightvision.limelight.frc.ControlMode.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-// public class LimeLight {
-//     NetworkTable masterTable = NetworkTableInstance.getDefault().getTable("limelight");
-//     NetworkTableEntry tx = masterTable.getEntry("tx");
-//     NetworkTableEntry ty = masterTable.getEntry("ty");
-//     NetworkTableEntry ta = masterTable.getEntry("ta");
+public class LimeLight {
+    NetworkTable masterTable = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = masterTable.getEntry("tx");
+    NetworkTableEntry ty = masterTable.getEntry("ty");
+    NetworkTableEntry ta = masterTable.getEntry("ta");
 
-//     NetworkTableEntry tv = masterTable.getEntry("tv");
+    NetworkTableEntry tv = masterTable.getEntry("tv");
 
-//     public void setCamMode (int camMode) // 0 - Vision Processing, 1-DriverCam
-//     {
-//         masterTable.getEntry("camMode").setValue(camMode);
-//     }
+    public void setCamMode (int camMode) // 0 - Vision Processing, 1-DriverCam
+    {
+        masterTable.getEntry("camMode").setValue(camMode);
+    }
 
 
-//     //read values periodically
-//     public double getHorixDegreeToTarget () {
-//         double x = tx.getDouble(0.0);
-//         return x;
-//     }
-//     public double getVerticalD () {
-//         double y = ty.getDouble(0.0);
-//         return y;
-//     }
-//     public double getArea () {
-//         double area = ta.getDouble(0.0);
-//         return area;
-//     }
-//     public double foundTarget () {
-//         return tv.getDouble(0) ;
-//     } // 1 is true, 0 is false
+    //read values periodically
+    public double getHorixDegreeToTarget () {
+        double x = tx.getDouble(0.0);
+        return x;
+    }
+    public double getVerticalD () {
+        double y = ty.getDouble(0.0);
+        return y;
+    }
+    public double getArea () {
+        double area = ta.getDouble(0.0);
+        return area;
+    }
+    public double foundTarget () {
+        return tv.getDouble(0) ;
+    }
+    
+    public void setDriverMode ()
+    {
+        setCamMode(1);
+    }
+    
+    // 1 is true, 0 is false
 
-//     //post to smart dashboard periodically
+    //post to smart dashboard periodically
 
-//     // SmartDashboard.putNumber("LimelightX", x);
-//     // SmartDashboard.putNumber("LimelightY", y);
-//     // SmartDashboard.putNumber("LimelightArea", area);
-// }
+    // SmartDashboard.putNumber("LimelightX", x);
+    // SmartDashboard.putNumber("LimelightY", y);
+    // SmartDashboard.putNumber("LimelightArea", area);
+}
