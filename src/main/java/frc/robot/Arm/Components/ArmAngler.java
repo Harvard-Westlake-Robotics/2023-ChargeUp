2dafcfb1d607a111c2c5d0349a38d2666b79565d
AsianUser <90284651+AsianUser@users.noreply.github.com>
2023-03-05 16:09:45 -0800

    Update ArmAngler.java

--- src/main/java/frc/robot/Arm/Components/ArmAngler.java
+++ src/main/java/frc/robot/Arm/Components/ArmAngler.java
@@ -1,6 +1,12 @@
 // package frc.robot.Arm.Components;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Encoder;
 
+<<<<<<< Updated upstream
 // // motor group
 // public class ArmAngler {
 //     SparkMax arm1;
@@ -11,6 +17,18 @@
 //         this.arm1 = arm1;
 //         this.arm2 = arm2;
 //         this.encoder = armEncoder;
+=======
+// motor group
+public class ArmAngler {
+    CANSparkMax arm1;
+    SparkMax arm2;
+    Encoder encoder;
+
+    public ArmAngler(int arm1, int arm2, Encoder armEncoder) {
+        this.arm1 = new SparkMax(8, false, true);
+        this.arm2 = new SparkMax(9, false, true);
+        this.encoder = new Encoder(4, 5, true);
+>>>>>>> Stashed changes
 
 //         arm1.setBrake(true);
 //         arm2.setBrake(true);
