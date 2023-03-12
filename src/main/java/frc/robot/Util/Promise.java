3677476f0af29a13b90979b3d253e56c00b1f0c0
OG Wolverine <113261036+HarvardWestlake@users.noreply.github.com>
2023-03-12 10:34:49 -0700

    working on promises

---
+++ src/main/java/frc/robot/Util/Promise.java
@@ -0,0 +1,41 @@
+package frc.robot.Util;
+
+import java.util.ArrayList;
+
+public class Promise {
+    boolean resolved = false;
+    ArrayList<Lambda> thens = new ArrayList<Lambda>();
+
+    public void then(Lambda then) {
+        if (resolved)
+            then.run();
+        else
+            thens.add(then);
+    }
+
+    public Promise then(Getter<Promise> then) {
+        if (resolved) {
+            return then.get();
+        } else {
+            Container<Promise> nextProm = new Container<Promise>(null);
+            Promise retProm = new Promise();
+
+            thens.add(() -> {
+                nextProm.val = then.get();
+            });
+
+            this.then
+
+            return retProm;
+        }
+    }
+
+    public void resolve() {
+        if (!resolved) {
+            resolved = true;
+            for (var exec : thens) {
+                exec.run();
+            }
+        }
+    }
+}
